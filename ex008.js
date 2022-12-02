7cbfabec29acb4ed333711c9a2724b274e022ac2
giorkakuda <113476281+giorkakuda@users.noreply.github.com>
2022-12-02 20:59:29 -0300

    conditions 1

---
+++ ex008.js
@@ -0,0 +1,7 @@
+var vel = 50
+console.log('a velocidade do seu carro é de '+vel+ 'km/h')
+if (vel > 60) { //condição simples
+    console.log('MULTADO! voce ultrapassou a velocidade permitida')
+}   
+console.log('boa viagem!!!')
+
---
+++ ex009.js
@@ -0,0 +1,10 @@
+var pais = 'Brazil'
+console.log('vivendo em ' +pais)
+if (pais == 'Brazil') {
+    console.log('Brasileiro')
+}else{
+    console.log('Estrangeiro')
+}
+    
+
+
