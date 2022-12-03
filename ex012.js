c0c69075cbc0c47c1604d7c9912ffe0fcde7700e
giorkakuda <113476281+giorkakuda@users.noreply.github.com>
2022-12-03 12:57:42 -0300

    condições multiplas e aninhadas

---
+++ ex012.js
@@ -0,0 +1,11 @@
+var idade = 17
+console.log ('vc tem '+ idade + ' anos')
+if (idade < 16) {
+    console.log ('não vota')
+} else if (idade < 18 || idade > 65) {
+        console.log('nao obrigatorio')
+    }
+    else {
+        console.log ('vota')
+    }
+    
