c95a56e27ac22bd5a272a15b2e3ecb3135ed4c4b
giorkakuda <113476281+giorkakuda@users.noreply.github.com>
2022-12-04 15:56:01 -0300

    looping (do, while)

--- ex017.js
+++ ex017.js
@@ -1,5 +1,11 @@
-var c = 1
-while (c < 5) {
-    console.log('hello world')
-}
+/*var c = 1
+while (c <= 3) {
+    console.log('hello world '+c)
+    c++
+}*/
 
+var c = 1
+do {
+    console.log('hello world ' +c)
+    c++
+}  while(c <= 5)
