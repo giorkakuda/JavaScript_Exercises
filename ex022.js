56ec518f768b10bc38c26f297e1a8a86939ef9ec
giorkakuda <113476281+giorkakuda@users.noreply.github.com>
2022-12-05 16:56:22 -0300

    ex022

---
+++ ex022.js
@@ -0,0 +1,14 @@
+/*function parimpar(n){
+    if (n % 2 == 0) {
+        return 'par'
+    } else {
+        return 'impar'
+        }       
+}
+var res = parimpar(18)
+console.log(res)*/
+
+function soma(n1=0, n2=0){
+    return n1+n2
+}
+console.log (soma (2,5))
