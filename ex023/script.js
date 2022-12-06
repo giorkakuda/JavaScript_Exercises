ff272dfcc052b4d75fa6d38482ba9d487dbe60ac
giorkakuda <113476281+giorkakuda@users.noreply.github.com>
2022-12-06 13:28:38 -0300

    analisador de numeros

--- ex023/script.js
+++ ex023/script.js
@@ -4,12 +4,18 @@ let res = document.getElementById("res")
 let valores = []
 
 function adicionar() {
-    if (isnumero(num.value) && !inlista(num.value, valores)) {
-        window.alert('tudo ok')
+    if (isnumero(num.value) && !inlista(num.value, valores)) {// exclamação significa 'não'
+        valores.push(Number(num.value))
+        let item = document.createElement("option")
+        item.text = 'valor '+num.value+' adicionado'
+        lista.appendChild(item)
+        res.innerHTML = ''
 
     } else {
         window.alert('Valor invalido ou ja encontrado')
     }
+    num.value = ''
+    num.focus()
 }
 
 function isnumero(n){
@@ -27,4 +33,25 @@ function inlista(n, l){
         return false
     }
 
+}
+
+function finalizar() {
+    if (valores.length ==0){
+        window.alert('Adicione valores antes de finalizar')
+    } else {
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        for (let pos in valores) {
+            if (valores[pos] > maior)
+                maior = valores[pos]
+            if (valores[pos] < menor)
+                menor = valores[pos]
+        }
+
+        res.innerHTML = ''
+        res.innerHTML += '<p> Ao todo temos '+tot+ ' numeros cadastrados </p>'
+        res.innerHTML += '<p> o MAIOR valor informado foi '+maior+ ' </p>'
+        res.innerHTML += '<p> o MENOR valor informado foi '+menor+ ' </p>'
+    }
 }
